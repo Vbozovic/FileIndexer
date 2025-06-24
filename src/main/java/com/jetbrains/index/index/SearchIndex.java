dba84d9a037ee79fc33c08813217b8b4e4d53a71
Vuk Bozovic <vbozovic14@yahoo.com>
2025-06-24 16:35:01 +0200

    Remove unnecessary method

--- src/main/java/com/jetbrains/index/index/SearchIndex.java
+++ src/main/java/com/jetbrains/index/index/SearchIndex.java
@@ -1,9 +1,7 @@
 package com.jetbrains.index.index;
 
-import java.io.File;
 import java.util.List;
 
 public interface SearchIndex {
     List<String> findWord(String word);
-    List<File> findWordFile(String word);
 }
