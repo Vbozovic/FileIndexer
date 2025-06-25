d9cfc077d8156dd31abff672f5be135f467056b4
Vuk Bozovic <vbozovic14@yahoo.com>
2025-06-25 12:48:34 +0200

    Add Interactive Main

--- src/main/java/com/jetbrains/index/Main.java
+++ src/main/java/com/jetbrains/index/Main.java
@@ -1,21 +1,73 @@
 package com.jetbrains.index;
 
+import com.jetbrains.index.index.IndexSearchService;
+import com.jetbrains.index.token.factory.CachingTokenFactory;
+import com.jetbrains.index.token.tokenizer.WhiteSpaceTokenizer;
 import com.jetbrains.index.watcher.FileSystemWatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws InterruptedException {
-        FileSystemWatcher watcher = new FileSystemWatcher(List.of("/Users/vbozovic/usavrsavanje/FileIndexer/src/main/resources"));
-        watcher.start();
-        while (true) {
-            Thread.sleep(0);
+    public static void main(String[] args) {
+        List<String> paths = new ArrayList<>();
+        for (String arg : args) {
+            if (arg.startsWith("-p")) {
+                paths.add(arg.substring("-p".length()));
+            }
+        }
+
+        try (FileSystemWatcher watcher = new FileSystemWatcher(paths)) {
+            IndexSearchService svc = new IndexSearchService(new WhiteSpaceTokenizer(CachingTokenFactory.getInstance()));
+            watcher.registerListener(svc);
+            watcher.start();
+
+            Scanner scanner = new Scanner(System.in);
+            CommandMode currentMode = CommandMode.MENU;
+            System.out.println("In menu:");
+            do {
+                String line;
+
+                switch (currentMode) {
+                    case MENU -> {
+                        System.out.println("Available commands: search,menu(to go back to the menu)");
+                        currentMode = nextMode(scanner.next());
+                    }
+                    case QUERY -> {
+                        line = scanner.next();
+                        if (nextMode(line) != CommandMode.MENU) {
+                            var found = svc.findWord(line);
+                            System.out.println("Result: ");
+                            found.forEach(System.out::println);
+                        } else {
+                            currentMode = CommandMode.MENU;
+                        }
+                    }
+                }
+
+            } while (scanner.hasNextLine());
         }
     }
+
+
+    private static CommandMode nextMode(String line) {
+        return switch (line) {
+            case "search" -> CommandMode.QUERY;
+            case "menu" -> CommandMode.MENU;
+            case null, default -> CommandMode.NONE;
+        };
+    }
+
+    private enum CommandMode {
+        MENU, QUERY, NONE
+    }
+
 
 }
